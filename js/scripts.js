f194312ab38b6e0846327865300944868b6df7d1
K3NTd02 <146251438+K3NTd02@users.noreply.github.com>
2023-11-13 16:10:13 -0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,18 @@ function createMap(){
     getData();
 };
 
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
 //function to retrieve the data and place it on the map
 function getData(){
     //load the data
@@ -25,9 +37,27 @@ function getData(){
             return response.json();
         })
         .then(function(json){
+            L.geoJson(json, {
+                onEachFeature: onEachFeature
+            }).addTo(map);            
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
             //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json).addTo(map);
-        })
+            L.geoJson(json, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        });
 };
 
 document.addEventListener('DOMContentLoaded',createMap)
+
+//codes from Chapter 4 Lesson 2 in Workbook
